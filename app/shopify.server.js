435cf7df29e610d7825748033536bc067f1a7b0c
Pranav Chavda <pranav@idrinkcoffee.com>
2024-05-25 12:55:38 -0400

    netlify plugin remove

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -14,7 +14,7 @@ const shopify = shopifyApp({
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.April24,
   scopes: process.env.SCOPES?.split(","),
-  appUrl: process.env.SHOPIFY_APP_URL || "",
+  appUrl: process.env.SHOPIFY_APP_URL || "Hello World",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
